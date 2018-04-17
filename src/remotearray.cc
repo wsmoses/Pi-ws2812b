6f49df14c12a4aa93fba730559ea78694502c21c
William S. Moses <gh@wsmoses.com>
2018-04-17 01:24:24 -0700

    Fix for windows

--- src/remotearray.cc
+++ src/remotearray.cc
@@ -3,23 +3,21 @@
 #include <string>
 #include <iostream>
 
+#ifdef _WIN32
+#include <windows.h> 
+#else
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#endif
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
 namespace py = pybind11;
 
@@ -41,11 +39,24 @@ public:
   };
 
 private:
-  void init_network() {
+#if _WIN32
+	WSADATA wsaData;
+#endif
+	void init_network() {
+#if _WIN32
+	  int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	  if (iResult != 0) {
+		  printf("WSAStartup failed with error: %d\n", iResult);
+		  exit(1);
+	  }
+#endif
     /* socket: create the socket */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serveraddr;
     if (sockfd < 0) {
+#if _WIN32
+		printf("found windows error %d\n", WSAGetLastError());
+#endif
       perror("Error opening socket");
       exit(1);
     }
@@ -58,10 +69,9 @@ private:
     }
 
     /* build the server's Internet address */
-    bzero((char *) &serveraddr, sizeof(serveraddr));
+    memset((char *) &serveraddr, 0, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr,
-    (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    memcpy((char *)&serveraddr.sin_addr.s_addr, (const char *)server->h_addr, server->h_length);
     serveraddr.sin_port = htons(_port);
 
     /* connect: create a connection with the server */
@@ -73,7 +83,7 @@ private:
   }
 
   void send_data(void* dat, size_t sz) {
-    int n = write(sockfd, dat, sz);
+    int n = send(sockfd, (const char*)dat, sz, 0);
     if (n < 0) {
       perror("Error writing");
       exit(1);
